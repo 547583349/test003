7fc3c5dc231ad242d95bacb843467413a58ef4c2
huangboying <huangboying123@qq.com>
2019-03-24 22:51:39 +0800

    first commit

---
+++ src/main/java/com/wangyi/springcloudzipkinserverdemo/SpringCloudZipkinServerDemoApplication.java
@@ -0,0 +1,17 @@
+package com.wangyi.springcloudzipkinserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+//@EnableZipkinServer
+@EnableZipkinStreamServer
+public class SpringCloudZipkinServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipkinServerDemoApplication.class, args);
+	}
+
+}
+
